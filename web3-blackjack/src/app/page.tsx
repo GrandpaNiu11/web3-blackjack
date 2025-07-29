d00f991db611bc3815580354a408067d1b0e2b14
szr <1061312375@qq.com>
2025-07-29 23:09:40 +0800

    扑克前端提交

--- web3-blackjack/src/app/page.tsx
+++ web3-blackjack/src/app/page.tsx
@@ -1,48 +1,51 @@
 'use client'
 
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import {useEffect, useState} from "react";
 
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+function Page() {
+    const ranks = ["A" ,"2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "J" , "Q" , "K"]
+    const suits = ["♥️" , "♠️" , "♣️" , "♦️"]
+    const cards = ranks.map(rank => suits.map(suit => ({ rank, suit }))).flat()
+    const [deck, setDeck] = useState< { rank: string, suit: string }[]>([])
+    useEffect(()=>{
+        setDeck(cards)
+    },[cards])
 
-  return (
-    <>
-      <div>
-        <h2>Account</h2>
 
+    return (<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl bold">Welcome to Web3 game Balck jak</h1>
+        <h2 className="text-2xl"> Message: Player wins /dealer wins :BlackJack /bust</h2>
+        <div className="mt-4">
+            <h2 >Dealers hand</h2>
+
+            <div className="flex  flex-row gap-2  ">
+                {deck.slice(0,3).map((card, index) => (
+                    <div key={index} className="w-32 h-42 bg-amber-300 border-black  rounded-md border-1 flex  justify-between flex-col">
+                            <p className="text-2xl self-start p-2" >{card.rank}</p>
+                            <p className="text-2xl self-center p-2">{card.suit}</p>
+                            <p className="text-2xl self-end p-2">{card.rank}</p>
+                    </div>
+                ))}
+
+            </div>
+        </div>
         <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
+            <h2>player hand</h2>
+            <div className="flex  flex-row gap-2  ">
+                <div className="w-32 h-42 bg-amber-300 border-black  rounded-md border-1"></div>
+                <div className="w-32 h-42 bg-amber-300 border-black  rounded-md border-1"></div>
+                <div className="w-32 h-42 bg-amber-300 border-black  rounded-md border-1"></div>
+            </div>
         </div>
 
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
+        <div className="flex  flex-row gap-2  mt-4">
+            <button className="bg-amber-300 rounded-md p-2" >Deal</button>
+            <button className="bg-amber-300 rounded-md p-2" >Hit</button>
+            <button className="bg-amber-300 rounded-md p-2" >Stand</button>
+        </div>
 
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+
+    </div>)
 }
 
-export default App
+export default Page
